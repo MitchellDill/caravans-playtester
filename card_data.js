9a104bfe975d042c10de6e7a5458fbee81a06920
Mitchell <malexanderdill@gmail.com>
2019-05-17 14:13:22 -0500

    Fix first round of basic debugging errors
    Small stuff like making the dice actually work, and had to cut out the self-referential
    enemy moveset stuff for the time being. I know there's a good way to approach it but
    it's not important as the focus for now!

--- card_data.js
+++ card_data.js
@@ -1,9 +1,29 @@
+//traits
+
+var allTraits = {
+	'hardy' : {
+		name : 'Hardy',
+		available : false,
+		inCombat : false,
+		inTown : false,
+		inTravel : false,
+		inGen : true,
+		effGen : function() {
+			this.stam += 2;
+			this.maxStam += 2;
+		} 
+	}
+};
+
+
+
 //hero cards
 var allHeroes = [];
 
 var Hero = function(name, stam, trait) {
 	this.name = name;
 	this.trait = allTraits[trait];
+	this.maxStam = stam;
 	this.stam = stam;
 	this.armor = 0;
 	this.guard = 0;
@@ -29,30 +49,92 @@ Hero.prototype.checkRoll = function(move, diceResults) {
 
 //hero generation zone
 
-var heroMulholland = new Hero('Our Grave-Minded Quartermaster, Mulholland', 16);
+var heroMulholland = new Hero('Our Grave-Minded Quartermaster, Mulholland', 16, 'hardy');
 
 
 
 
-//traits
 
-var allTraits = {
 
+//movelist
+
+var allMoves = {
+	sword : [],
+	axe : [],
+	hammer : [],
+	dagger : [],
+	spear : [],
+	rapier : [],
+	bow : [],
+	shield : []
 };
+
+var costCeiling = 7;
+
+(function (allMoves, costCeiling) {
+	for (var weapon in allMoves) {
+		for (var i = 0; i < costCeiling + 1; i++) {
+		  allMoves[weapon].push({costOfMoves : i});
+		};
+	};
+})(allMoves, costCeiling);
+
+
+var Move = function(name, cost, weaponType, enemy, self, extra) {
+	this.name = name;
+	this.cost = cost;
+	this.weaponType = weaponType;
+	this.enemy = enemy;
+	this.self = self;
+	this.extra = extra;
+
+	allMoves[weaponType][cost][name] = this;
+};
+
+
+Move.prototype.attack = function(target, weapon, hero) {
+	var currentTarget = target.getPrototypeOf();
+	return this[currentTarget](target, weapon, hero);
+};
+
+
+
+
+
+//movelist -- move creation
+
+var swordMoveHack = new Move('hack', 1, 'sword', function(target){
+	return (target.howManyBreak);
+});
+var swordMoveSlash = new Move('slash', 3, 'sword', function(target, weapon) {
+	return (weapon.damage * 2);
+});
+
+
+
+
 
 
 
 //weapon cards
 
-var allWeapons = [];
+var allWeapons = {
+	allSwords : [],
+	allAxes : [],
+	allHammers : [],
+	allDaggers : [],
+	allSpears : [],
+	allBows : [],
+	allShields : []
+}
 
-var allSwords = [];
-var allAxes = [];
-var allHammers = [];
-var allDaggers = [];
-var allSpears = [];
-var allBows = [];
-var allShields = [];
+
+var moveFinder = function(type, name) {
+	return allMoves[type].findIndex(function(costObj) {
+		return (costObj[name]);
+	});
+};
+
 
 var Weapon = function(name, type, damage, twoHanded, worth, move1, move2, move3) {
 	this.name = name;
@@ -63,16 +145,16 @@ var Weapon = function(name, type, damage, twoHanded, worth, move1, move2, move3)
 	this.available = true;
 
 	var move1Group = moveFinder(type, move1); 
-	this.move1 = allMoves[type][move1Group].move1;
+	this.move1 = allMoves[type][move1Group][move1];
 
 	if (move2) {
 		var move2Group = moveFinder(type, move2); 
-		this.move1 = allMoves[type][move2Group].move2;
+		this.move1 = allMoves[type][move2Group][move2];
 	}
 
 	if (move3) {
 		var move3Group = moveFinder(type, move3); 
-		this.move1 = allMoves[type][move3Group].move3;
+		this.move1 = allMoves[type][move3Group][move3];
 	}
 
 	var typePush = type[0].toUpperCase() + type.slice(1);
@@ -83,66 +165,10 @@ var Weapon = function(name, type, damage, twoHanded, worth, move1, move2, move3)
 
 var unwieldyGreatsword = new Weapon('Unwieldy Greatsword', 'sword', 3, true, 99, 'hack', 'slash');
 
-allWeapons.push(allSwords, allAxes, allHammers, allDaggers, allSpears, allBows, allShields);
 
 
 
-//movelist
 
-var Move = function(name, cost, enemy, self, extra) {
-	this.name = name;
-	this.cost = cost;
-	this.enemy = enemy;
-	this.self = self;
-	this.extra = extra;
-};
-
-Move.prototype.attack = function(target, weapon, hero) {
-	var currentTarget = target.getPrototypeOf();
-	return this[currentTarget](target, weapon, hero);
-};
-
-var moveFinder = function(type, name, abilCost) {
-	var positiveCostsArray = allMoves[type].filter(function(costObj) {
-		return costObj[name];
-	});
-	if (abilCost) {
-		return positiveCostsArray.filter(function(costObj) {
-			return (costObj === type + 'Cost' + abilCost);
-		});
-	}
-	return positiveCostsObj[0];
-};
-
-var allMoves = {
-	sword : [swordCost0, swordCost1, swordCost2, swordCost3];
-	axe : [],
-	hammer : [],
-	dagger : [],
-	spear : [],
-	rapier : [],
-	bow : [],
-	shield : []
-};
-
-
-var swordCost0 = {};
-var swordCost1 = {
-	hack : swordMoveHack
-};
-var swordCost2 = {};
-var swordCost3 = {
-	slash : swordMoveSlash
-};
-
-//movelist -- move creation
-
-var swordMoveHack = new Move('Hack', 1, function(target){
-	return (target.howManyBreak);
-});
-var swordMoveSlash = new Move('Slash', 3, function(target, weapon) {
-	return (weapon.damage * 2);
-});
 
 
 
@@ -157,7 +183,7 @@ var Dice = function() {
 };
 
 Dice.prototype.roll = function() {
-	return this.sides[Math.random() * this.sides.length];
+	return this.sides[Math.floor(Math.random() * this.sides.length)];
 }
 
 var startDice = new Dice(0, 1, 2);
@@ -190,20 +216,17 @@ var Enemy = function(name, stam, tier, region, breakThresh) {
 	this.engaged = false;
 	this.statusEffect = false;
 	this.statuses = [];
-	/*
-	this.moveset = allEnemyMoves[region][tier][name];
-	*/
 	this.ultCharge = 0;
 	this.reward = [];
 
 	allEnemies[region][tier].push(this);
 };
 
-Enemy.protoype.howManyBreak = function() {
+Enemy.prototype.howManyBreak = function() {
 	return this.breakDiceCurrent.length;
 };
 
-Enemy.protoype.breakDiceTotal = function() {
+Enemy.prototype.breakDiceTotal = function() {
 	return this.breakDiceCurrent.reduce(function(accum, current) {
 		return accum + current;
 	}, 0);
@@ -215,6 +238,7 @@ Enemy.prototype.attack = function(target) {
 };
 
 var enemyMurkman = new Enemy('Murkman', 18, 2, 'Deep Marshes', 9);
+
 enemyMurkman.moveset = {
 	basic : function() {return 2},
 	specialized : function() {
@@ -225,9 +249,9 @@ enemyMurkman.moveset = {
 		this.stam += 6;
 		return 3;
 	},
-	basicText : 'deals ' + enemyMurkman.moveset.basic() + ' damage to ' + target.name,
+	basicText : 'deals 2 damage to ',
 	specializedText : 'slips out of engagement!',
-	advancedText : 'regenerates back to ' + this.stam + 'stamina, then deals ' + enemyMurkman.moveset.advanced() + ' damage to ' + target.name,
+	advancedText : 'regenerates back to ' + this.stam + 'stamina, then deals 3 damage to ',
 	ultimate : function() {
 		return party.map(function(hero) {
 			var message = '';
@@ -238,17 +262,16 @@ enemyMurkman.moveset = {
 			if (!rollPass) {
 				rollMessage = 'was afflicted by bogrot!';
 				hero.statusEffect = true;
-				hero.statuses.push(bogrot);
+				hero.statuses.push(allStatuses.disease.bogrot);
 			}
 			return message = hero.name + ' rolled a ' + heroRoll + ' and ' + rollMessage;
 		})
-	} 
+	}, 
 	ultFiresOn : 3,
 	ultimateText : 'If heroes cannot roll a 4 or higher on a six-sided die, they\'ll be affliceted by bogrot!',
 	hasOppo : true,
 	oppo : function() {return [1]},
-	oppoText : 'Disengaging from the ' + this.name + 'will cause a hero to take ' + 
-		enemyMurkman.moveset.oppo()[0] + ' damage--this effect then ripples through the ranks of all' + 
+	oppoText : 'Disengaging from the ' + this.name + 'will cause a hero to take 1 damage--this effect then ripples through the ranks of all' + 
 		' other disengaged heroes!'
 };
 
@@ -275,11 +298,37 @@ enemy moveset template = {
 
 //status, diseases, boons, etc
 
+var allStatuses = {
+	disease : {
+		bogrot : {
+			name : 'bogrot',
+			inCombat : false,
+			inTown : false,
+			inTravel : true,
+			effTravel : function() {return 1},
+		}
+	}
+};
 
 
 
 
-
-//the engagement object
+//the combat and engagement object model
 
 var currentEngagements = {};
+
+var theParty = [];
+
+var theEnemyParty = [];
+
+var establishEngagement = function(hero, enemy) {
+	var heroString = hero.name;
+	var enemyString = enemy.name;
+	hero.engaged = true;
+	if (currentEngagements[enemyString]) {
+		currentEngagements[enemyString].push(heroString);
+	} else {
+		currentEngagements[enemyString] = [].push(heroString);
+		enemy.engaged = true;
+	}
+};
